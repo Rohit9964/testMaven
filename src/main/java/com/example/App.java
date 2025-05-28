c85d3456c0f975ba5e12199a6840518b5a0b231d
Rohit9964 <rohithonakhande2@gmail.com>
2025-05-28 21:07:59 +0530

    IC

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this the first maven app build for the test!" );
+    }
+}
